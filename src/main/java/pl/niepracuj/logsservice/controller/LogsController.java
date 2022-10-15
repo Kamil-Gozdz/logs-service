b3165e7ba3c0e855e68ef1166b745435ac18b130
Kamil <kamilgozdz23@gmail.com>
2022-10-15 13:05:32 +0200

    initial commit

---
+++ src/main/java/pl/niepracuj/logsservice/controller/LogsController.java
@@ -0,0 +1,23 @@
+package pl.niepracuj.logsservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+public class LogsController {
+
+    private final LogService logService;
+
+    private final LogMapper logMapper;
+
+    @PostMapping
+    public LogDto createLog(@RequestBody LogDto logdto){
+        return logMapper.toDto(logService.saveLog(logMapper.toNewEntity(logdto)));
+    }
+}
